94f40e753a7f18719ef9f131c0755563c0f39757
Tazio <tazioalmeida@gmail.com>
2024-07-30 16:33:40 -0300

    finish1parte

--- my-portfolio/src/pages/Home/sections/Hero/Hero.tsx
+++ my-portfolio/src/pages/Home/sections/Hero/Hero.tsx
@@ -1,44 +1,72 @@
-import { Button, Container, Grid, styled, Typography } from "@mui/material";
+import {Box, Container, Grid, styled, Typography } from "@mui/material";
 import Perfil from "../../../../assets/images/perfil.jpg";
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import ForwardToInboxIcon from '@mui/icons-material/ForwardToInbox';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 const Hero = () => {
-  const StyledHero = styled("div")(() => ({
-    backgroundColor: "black",
-    height: "100vh"
-  }));
-  const StyledImg = styled("img")(() => ({
-    width: "100%",
+  const StyledHero = styled("div")(({ theme }) => ({
+    backgroundColor: theme.palette.primary.main,
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    [theme.breakpoints.up('xs')]: { // <= mobile
+        paddingTop: "100px",
+
+    },
+    [theme.breakpoints.up('md')]: { // >=mobile
+        paddingTop: "0",
+    }
+}))
+
+const StyledImg = styled("img")(({ theme }) => ({
+    width: "75%",
     borderRadius: "50%",
-  }));
+    border: `1px solid ${theme.palette.primary.contrastText}`
+}))
 
   return (
     <>
-      <StyledHero>
-        <Container>
-
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={4}>
-          <StyledImg src={Perfil} />
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <Typography color="primary" variant="h1" textAlign="center">Tazio Arruda</Typography>
-            <Typography color="primary" variant="h2" textAlign="center">full stack web developer</Typography>
-            <Grid container>
-                <Grid item xs={12} nd={6}>
-                <Button><FileDownloadIcon/>Download CV</Button>
-                </Grid>
-                
-                <Grid item xs={12} nd={6}>
-                <Button><ForwardToInboxIcon/>Contact me</Button>
-                </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-       
-        </Container>
-      </StyledHero>
+         <StyledHero>
+                <Container maxWidth="lg">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={5}>
+                            <Box position="relative">
+                                <Box position="absolute" width={"150%"} top={-100} right={0}>
+                                    <AnimatedBackground />
+                                </Box>
+                                <Box position="relative" textAlign="center">
+                                    <StyledImg src={Perfil} />
+                                </Box>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={7}>
+                            <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Tazio Arruda</Typography>
+                            <Typography color="primary.contrastText" variant="h2" textAlign="center" >web developer full stack</Typography>
+                            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                    <StyledButton>
+                                        <FileDownloadIcon />
+                                        <Typography>
+                                            Download CV
+                                        </Typography>
+                                    </StyledButton>
+                                </Grid>
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                    <StyledButton>
+                                        <MailOutlineIcon/>
+                                        <Typography>
+                                            Contact me
+                                        </Typography>
+                                    </StyledButton>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </StyledHero>
     </>
   );
 };
